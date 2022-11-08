2b02807ca9c66abdf6a2cb3080580a5bc8b05347
MauryaRitesh <ritesh.k.m786@gmail.com>
2022-11-08 17:25:47 +0530

    fixes

--- src/main/java/com/example/softdart/DatabaseConnection.java
+++ src/main/java/com/example/softdart/DatabaseConnection.java
@@ -2,15 +2,30 @@ package com.example.softdart;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ResourceBundle;
 
 public class DatabaseConnection {
     public Connection databaseLink;
 
     public Connection getConnection(){
-        String databaseName = "account_db";
+        /*String databaseName = "account_db";
         String databaseUser = "root";
         String databasePassword = "abcd1234";
-        String url = "jdbc:mysql://localhost:3306/" + databaseName;
+        String url = "jdbc:mysql://localhost:3306/" + databaseName;*/
+
+        ResourceBundle rd = ResourceBundle.getBundle("auth");
+
+        // driver name for mysql
+        String databaseName = rd.getString("databaseName");
+
+        // url of the database
+        String url = rd.getString("url");
+
+        // username to connect db
+        String databaseUser = rd.getString("databaseUser");
+
+        // password to connect db
+        String databasePassword = rd.getString("databasePassword");
 
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
